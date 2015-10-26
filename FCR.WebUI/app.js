02a043a8a425a279a27e88c689618b6683a9c209
Wease007@github.com <bcrandall-github@spamarrest.com>
2015-10-26 13:28:52 -0500

    Added stuff to startup on solution

--- FCR.WebUI/app.js
+++ FCR.WebUI/app.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 var app;
 (function (app) {
     angular.module("fitnessApp", []);
 })(app || (app = {}));
-=======
-var app;
-(function (app) {
-    angular.module("fitnessApp", []);
-})(app || (app = {}));
->>>>>>> 89861ac61cce21a38e10f2366d7f794411928fc8
 //# sourceMappingURL=app.js.map
