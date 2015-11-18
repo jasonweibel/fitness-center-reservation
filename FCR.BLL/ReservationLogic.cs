e8d8370c46c165a3bc427c621f414ae1b4da3c2b
Jason Weibel <jason.weibel@pioneer.com>
2015-11-18 12:17:42 -0600

    changes

--- FCR.BLL/ReservationLogic.cs
+++ FCR.BLL/ReservationLogic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using FCR.DAL;
+using FCR.DomainModel;
 using FCR.WebApiModel;
 using cmn = FCR.BLL.Common;
 
@@ -19,30 +20,71 @@ namespace FCR.BLL
             _entities = entities;
         }
 
-        //public ExistingReservation GetReservation(int reservationId)
-        //{
-        //    var results = new List<ExistingReservation>();
-        //    var reservation = _entities.allReservations.First(x => x.reservationId == reservationId);
+        public ExistingReservation GetReservation(int reservationId)
+        {
+            var reservation = _entities.Reservations.FirstOrDefault(x => x.Id == reservationId);
 
-        //    MapReservationData(reservation, results);
-        //    return results.First();
-        //}
+            if (reservation == null) throw new ApplicationException("Reservation Does Not Exist.");
 
-        //public List<ExistingReservation> GetAllExistingReservations()
-        //{
-        //    var results = new List<ExistingReservation>();
+            var resource = _entities.Resources.FirstOrDefault(x => x.Id == reservation.Resource.Id);
 
-        //    var reservations = entities.allReservations
-        //        .Where(x => x.reservationDate >= _defaultStartDate && x.reservationDate < _defaultEndDate)
-        //        .ToList();
+            if (resource == null) throw new ApplicationException("Resources Does Not Exist.");
 
-        //    reservations.ForEach(x =>
-        //    {
-        //        MapReservationData(x, results);
-        //    });
+            var center = _entities.FitnessCenters.FirstOrDefault(x => x.Id == resource.FitnessCenter.Id);
 
-        //    return results;
-        //}
+            if (center == null) throw new ApplicationException("FitnessCenters Does Not Exist.");
+
+            // TODO - This is wrong...
+            var sites = _entities.Sites.FirstOrDefault(x => x.Id == center.Id);
+
+            if (sites == null) throw new ApplicationException("Sites Does Not Exist.");
+
+            var user = _entities.User.FirstOrDefault(x => x.Id == reservation.User.Id);
+
+            if (user == null) throw new ApplicationException("User Does Not Exist.");
+
+            var resourceType = _entities.ResourceTypes.FirstOrDefault(x => x.Id == resource.ResourceType.Id);
+
+            if (user == null) throw new ApplicationException("User Does Not Exist.");
+
+            return ExistingReservationMapper(reservation, resource, resourceType, user, center);
+        }
+
+        public List<ExistingReservation> GetAllExistingReservations()
+        {
+            //var results = new List<ExistingReservation>();
+
+            //var reservations = entities.allReservations
+            //    .Where(x => x.reservationDate >= _defaultStartDate && x.reservationDate < _defaultEndDate)
+            //    .ToList();
+
+            //reservations.ForEach(x =>
+            //{
+            //    MapReservationData(x, results);
+            //});
+
+            var reservation = _entities.Reservations;
+            var resource = _entities.Resources;
+            var center = _entities.FitnessCenters;
+            var sites = _entities.Sites;
+            var user = _entities.User;
+            var resourceType = _entities.ResourceTypes;
+
+            var reservations = from r in reservation
+                from re in resource.Where(x => x.Id == r.Resource.Id).DefaultIfEmpty()
+                from c in center.Where(x => x.Id == re.FitnessCenter.Id).DefaultIfEmpty()
+                from s in sites.Where(x => x.Id == c.Id).DefaultIfEmpty()
+                // TODO This is wrong too.
+                from u in user.Where(x => x.Id == r.User.Id).DefaultIfEmpty()
+                from rt in resourceType.Where(x => x.Id == re.ResourceType.Id)
+                select new List<ExistingReservation>()
+                {
+                    ExistingReservationMapper(r, re, rt, u, c)
+                };
+
+            // TODO - figure this out.
+            return reservations;
+        }
 
         //public List<ExistingReservation> GetExistingReservations(int fitnessCenterId)
         //{
@@ -114,6 +156,26 @@ namespace FCR.BLL
         //    results.Add(row);
         //}
 
+        private static ExistingReservation ExistingReservationMapper(Reservation reservation, Resource resource, ResourceType resourceType, User user, FitnessCenter center)
+        {
+            return new ExistingReservation()
+            {
+                Id = reservation.Id,
+                DayId = cmn.GetDayId(reservation.ReservationDate),
+                EquipmentId = resource.Id,
+                EquipmentName = resource.ResourceName,
+                EquipmentTypeDescription = resourceType.ResourceTypeDescription,
+                EquipmentTypeId = resource.ResourceType.Id,
+                FirstName = user.FirstName,
+                FitnessCenterId = center.Id,
+                IntervalLength = cmn.IntervalLength(center.Id), 
+                LastName = user.LastName,
+                StartMinutesSinceMidnight = cmn.IntervalLength(center.Id) * reservation.ReservationTimeUnitStart, 
+                StartTime = cmn.GetDateTime(reservation.ReservationDate, reservation.ReservationTimeUnitStart, center.Id).ToLongTimeString(),
+                UserId = user.Id,
+                UserName = user.UserName
+            };
+        }
 
 
     }
